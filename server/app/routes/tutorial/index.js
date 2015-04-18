11c9ece39df2d28231ebf1d4d13a4028c99f918e
gskerry <gregory.skerry@gmail.com>
2015-04-18 18:44:04 -0400

    server routes

--- server/app/routes/tutorial/index.js
+++ server/app/routes/tutorial/index.js
@@ -7,3 +7,23 @@ module.exports = router;
 router.get('/videos', function (req, res) {
     res.send(VIDEOS);
 });
+
+
+// ******************************************
+// Need an 'internal API endpoint' for films
+// *******************************************
+
+var FilmsModel = require('./models/flash-card-model')
+
+router.get('/films', function (req, res) {
+    
+    var modelParams = req.query.category ? { category: req.query.category } : {};
+
+    FlashCardModel.find(modelParams, function (err, cards) {
+        setTimeout(function () {
+            res.send(cards);
+        }, Math.random() * 1000);
+    });
+
+});
+
