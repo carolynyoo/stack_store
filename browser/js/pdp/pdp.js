b22b866f567990493f2f17089b9a7acd0775c4f3
Carolyn Yoo <yoo.carolyn@gmail.com>
2015-04-18 18:41:04 -0400

    Wrote initial factory and controller

---
+++ browser/js/pdp/pdp.js
@@ -0,0 +1,32 @@
+'use strict';
+app.config(function ($stateProvider) {
+    $stateProvider.state('pdp', {
+        url: '/products/:pid',
+        templateUrl: 'js/pdp/pdp.html',
+        controller: 'PdpCtrl',
+        resolve: {
+            pdpInfo: function ($stateParams, pdpFactory) {
+               // return pdpFactory.getInfo($stateParams.pid);
+               // Matrix test for now - do not have category view wired up yet 
+               return pdpFactory.getInfo('5532c18a0dad5827ab24af49');
+            }
+        }
+    });
+});
+
+app.factory('pdpFactory', function ($http) {
+    return {
+        getInfo: function () {
+            return $http.get('/products/:id').then(function (response) {
+                return response.data;
+            });
+        }
+    };
+});
+
+app.controller('PdpCtrl', function ($scope, pdpInfo) {
+  $scope.film = pdpInfo; 
+  
+  // retrieve category names from object ids later
+  $scope.categories = getCategories($scope.film.categories);
+})
