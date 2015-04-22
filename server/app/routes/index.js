b8f03851d2cf5393e065187a0cb2378b9e68a5fe
Alex Liu <yuningalexliu@gmail.com>
2015-04-21 20:03:13 -0400

    can successfully create new users

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,6 +1,17 @@
 'use strict';
 var router = require('express').Router();
 module.exports = router;
+var mongoose = require('mongoose');
+
+// Import User model;
+var User = mongoose.model('User');
+
+router.post('/new-user', function(req, res) {
+	var user = new User(req.body);
+	user.save(function (err, data) {
+		  if (err) return console.error(err);
+	});
+});
 
 router.use('/tutorial', require('./tutorial'));
 router.use('/members', require('./members'));
