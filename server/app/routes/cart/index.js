339fa460d35be3ff8e25756ddc48d78bdc681d4c
Alex Liu <yuningalexliu@gmail.com>
2015-04-25 16:32:56 -0400

    a little cleanup

--- server/app/routes/cart/index.js
+++ server/app/routes/cart/index.js
@@ -9,16 +9,6 @@ router.post('/', function (req, res, next) {
 	var filmId = req.body.filmId;
 	var sessionId = req.sessionID;
 
-	//Check if cart already exists, if it doesn't create a new cart
-	// function findCartBasedOnSessionId (sessionId) {
-	// 	cartModel.find({sessionId: sessionId}, function(err, existingCart) {
-	// 		if (err) {return console.err(err)};
-	// 		// console.log('Finding cart');
-	// 		console.log(existingCart[0]);
-	// 		return existingCart[0];
-	// 	})
-	// 	return existingCart[0];
-	// }
 	cartModel.findOne({sessionId: sessionId}).exec()
 	.then(function (cart) {
 		console.log(cart);
@@ -36,25 +26,4 @@ router.post('/', function (req, res, next) {
 	  	res.sendStatus(200);
 	});
 
-	// findCartBasedOnSessionId(sessionId);
-
-	// console.log(findCartBasedOnSessionId(sessionId));
-
-	// console.log(newCart);
-
-	// if (findCartBasedOnSessionId(sessionId) == undefined) {
-	// 	cart = new cartModel({sessionId: sessionID});
-	// } else {
-	// 	cart = findCartBasedOnSessionId(sessionId);
-	// }
-
-	// cart.films.push(filmId);
-
-	//But if it does, simply update the array of films
-
-	//Save the cart
-	// cart.save(function (err) {
-	//   if (err) return handleError(err);
-	//   console.log('Cart model updated!')
-	// });
 })
