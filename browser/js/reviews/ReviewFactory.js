62ed0fd5f23cac6ab7b8fda4ff9a066c5b5c107f
darksim735 <darksim735@gmail.com>
2015-05-02 14:44:09 -0400

    Started routing and template to write review from order page

--- browser/js/reviews/ReviewFactory.js
+++ browser/js/reviews/ReviewFactory.js
@@ -9,3 +9,25 @@ app.factory('ReviewFactory', function($http) {
         }
     };
 });
+
+
+app.controller('ReviewCtrl', function ($scope, $http, $stateParams, $state, pdpInfo) {
+  $scope.film = pdpInfo; 
+
+  $scope.addReviewToFilm = function() {
+    $http.post('/api/review', {filmId: $stateParams.pid}).
+    success(function() {
+    	// Get data from form fields
+    	// Rating
+    	// Comment
+
+        console.log("Review added to Film!");
+        $state.go('home');
+    }).
+    error(function() {
+        console.log("Error reviewing film");
+    });
+
+  };
+
+});
