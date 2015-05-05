2c821634fa2a459a259dc3d6a826e79686052fd2
gskerry <gregory.skerry@gmail.com>
2015-05-05 02:24:00 -0400

    init

---
+++ server/app/routes/home/index.js
@@ -0,0 +1,20 @@
+
+var mongoose = require('mongoose');
+var router = require('express').Router();
+module.exports = router;
+
+var FilmsModel = mongoose.model('Film');
+
+router.get('/', function (req, res) {
+    
+    
+	FilmsModel.find({})
+		.sort('-purchased')
+		.limit(3)
+		.populate('categories')
+		.exec(function(err, films){
+			if(err) throw err
+			res.json(films);
+		});
+
+});
