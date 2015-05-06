40cfdcd0d6f03aefa9778178086ac58885cf2854
gskerry <gregory.skerry@gmail.com>
2015-05-06 13:43:27 -0400

    routes setup, displaying top films on homepage (to clean up view)

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -2,6 +2,38 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        controller: 'homeCtrl'
     });
+});
+
+app.controller('homeCtrl', function ($scope, HomeFactory, CategoryFactory) {
+
+	$scope.getTop = function(){
+
+		HomeFactory.getFilms()
+		.then(function(filmsfromserver){
+		$scope.films = filmsfromserver;
+		console.log("$scope.films: ",$scope.films);
+		})
+		.catch(function(err){
+		console.log("error! : ",err);
+		}); 
+	};
+
+	$scope.getTop();
+
+});
+
+
+
+app.factory('HomeFactory', function ($http) {
+	return {
+		getFilms: function(category){
+			return $http.get('/api/home', {})
+				.then(function (response) {
+                	return response.data;
+            	});
+		}
+	}
 });
