9a8fc16566b51b970bc65395b10e8f688370dbdb
gskerry <gregory.skerry@gmail.com>
2015-05-07 18:41:42 -0400

    revert changes - match master

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -5,8 +5,7 @@ var mongoose = require('mongoose');
 var schema = new mongoose.Schema({
     email: {
         type: String,
-        required: true, 
-        unique: true
+        required: true
     },
     password: {
         type: String,
