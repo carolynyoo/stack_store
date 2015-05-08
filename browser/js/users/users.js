ab165b9fafc2a3cad2b343bfe9c5cc1a74d87323
Carolyn Yoo <yoo.carolyn@gmail.com>
2015-05-07 22:35:30 -0400

    Remove commented code

--- browser/js/users/users.js
+++ browser/js/users/users.js
@@ -22,28 +22,4 @@ app.controller('UserCtrl', function ($scope, AuthService, $state, $http, User) {
 
   $scope.get();
 
-    // $scope.error = null;
-    // $scope.newAccount = {};
-
-    // $scope.createAccount = function (newAccount) {
-
-    //   $scope.error = null;
-
-    //   $http.post('/api/new-user', newAccount).
-    //     success(function(data) {
-    //       console.log("New user successfully registered!");
-
-    //             //Automatically log in after creating a new account
-
-    //             AuthService.login({email: newAccount.email, password: newAccount.password}).then(function () {
-    //                 $state.go('home');
-    //             }).catch(function () {
-    //                 $scope.error = 'Invalid login credentials.';
-    //             });
-    //     }).
-    //     error(function(data) {
-    //       console.log("Some error occurred during account registration.");
-    //     });
-    // };
-
 });
